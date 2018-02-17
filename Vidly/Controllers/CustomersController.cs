79818b6bda137799b3aa42dec040208c5fe51670
IrwanHamid <fieldengineerirwan@gmail.com>
2018-02-17 16:02:35 +0800

    Add CustomerControllers with actions and views of Index and Detail. No styling was applied.

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private List<Customer> _customers;
+
+        public CustomersController()
+        {
+            _customers = new List<Customer>
+            {
+                new Customer { Id=1, Name="John Smith" },
+                new Customer { Id=2, Name="Marry William" }
+            };
+
+        }
+        
+        public ActionResult Index()
+        {
+            return View(_customers);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var customer = _customers.FirstOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+    }
+}
