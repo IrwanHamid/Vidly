a8f8a6677a6e7bf21f9f724fe222f026e1c72e8e
IrwanHamid <fieldengineerirwan@gmail.com>
2018-02-17 16:31:41 +0800

    Add Index and Detail action and view for MoviesController. No stling was applied. Also rectify _NavBar view to point to "Index" action for Movies link rather than "About".

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,6 +10,31 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        
+        private List<Movie> _movies;
+
+        public MoviesController()
+        {
+            _movies = new List<Movie>
+            {
+                new Movie{ Id=1, Name="Shrek" },
+                new Movie{ Id=2, Name="Wall-e" }
+            };
+
+        }
+
+        public ActionResult Index()
+        {
+            return View(_movies);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var movie = _movies.FirstOrDefault(m => m.Id==id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+        }
     }
 }
